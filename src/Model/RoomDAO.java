d1a571ec9917bf0c0bd5ed188c31dbc4dc6900cb
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-05-09 15:42:33 +0200

    getAllRooms and getAllAvailableRooms adeed in database handler

--- src/Model/RoomDAO.java
+++ src/Model/RoomDAO.java
@@ -59,4 +59,59 @@ public class RoomDAO extends DatabaseHandlerFactory
     }
     return temp;
   }
+
+  public ArrayList<Room> getAllAvailableRooms(Date startDate, Date endDate)
+  {
+    ArrayList<Room> temp = new ArrayList<>();
+    int currentRoomNumber = -1;
+    int incrementor = 0;
+    try (Connection connection = super.establishConnection())
+    {
+      PreparedStatement statement = connection.prepareStatement(
+          "SELECT Room.roomNumber, amenityName, RoomType.typeOfRoom, price, Room.roomNumber, stateOfRoom\n"
+              + "FROM Room LEFT OUTER JOIN Amenities ON (Room.roomNumber = Amenities.roomNumber), RoomType, Reservation\n"
+              + "WHERE Room.typeOfRoom = RoomType.typeOfRoom AND Room.roomNumber NOT IN (SELECT Room.roomNumber\n"
+              + "FROM Room, Reservation\n"
+              + "WHERE Room.roomNumber = Reservation.roomNumber AND (Reservation.startDate < ? OR Reservation.endDate > ?))\n"
+              + "ORDER BY Room.roomNumber;");
+      statement.setDate(1, endDate);
+      statement.setDate(2, startDate);
+      ResultSet rs = statement.executeQuery();
+      while (rs.next())
+      {
+        String amenity = rs.getString("amenityName");
+        String typeOfRoom = rs.getString("typeOfRoom");
+        double price = rs.getInt("price");
+        int roomNumber = rs.getInt("roomNumber");
+        String stateOfRoom = rs.getString("stateOfRoom");
+        if(roomNumber != currentRoomNumber)
+        {
+          if (!(currentRoomNumber == -1))
+          {
+            incrementor ++;
+          }
+          currentRoomNumber = roomNumber;
+          temp.add(new Room(typeOfRoom, price, roomNumber, stateOfRoom));
+          if(amenity != null)
+          {
+            temp.get(incrementor).addAmenities(amenity);
+          }
+        }
+        else
+        {
+          if(amenity != null)
+          {
+            temp.get(incrementor).addAmenities(amenity);
+          }
+        }
+      }
+    }
+    catch (SQLException e)
+    {
+      e.printStackTrace();
+    }
+    return temp;
+  }
+
+  
 }
