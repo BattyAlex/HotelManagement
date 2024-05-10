315f07acf7253078c6151923ad942fb36af948d8
BattyAlex <Alexandriaahansen@gmail.com>
2024-05-10 12:42:54 +0200

    Update ViewHandler.java

--- src/View/ViewHandler.java
+++ src/View/ViewHandler.java
@@ -1,13 +1,37 @@
 package View;
 
 import ViewModel.ViewModelFactory;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
 
 public class ViewHandler
 {
   private ViewFactory viewFactory;
+  private Scene currentScene;
+  private Stage primaryStage;
 
   ViewHandler(ViewModelFactory viewModelFactory)
   {
     this.viewFactory = new ViewFactory(this, viewModelFactory);
+    this.currentScene = new Scene(new Region());
+  }
+  public void start(Stage primaryStage)
+  {
+    this.primaryStage = primaryStage;
+    openView(ViewFactory.LOGIN);
+  }
+  public void openView(String id)
+  {
+    Region root = viewFactory.load(id);
+    currentScene.setRoot(root);
+    primaryStage.setTitle("Hotel Management System");
+    primaryStage.setScene(currentScene);
+    primaryStage.sizeToScene();
+    primaryStage.show();
+  }
+  public void closeView()
+  {
+    primaryStage.close();
   }
 }
