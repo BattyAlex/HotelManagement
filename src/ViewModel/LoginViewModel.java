ca414dbe78df77ff4771d889686654efb07b802c
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-05-10 12:28:06 +0200

    murder

--- src/ViewModel/LoginViewModel.java
+++ src/ViewModel/LoginViewModel.java
@@ -4,17 +4,43 @@ import Model.HotelModel;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-public class LoginViewModel
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class LoginViewModel implements PropertyChangeListener
 {
   private StringProperty username;
   private StringProperty password;
+  private StringProperty error;
   private final HotelModel model;
 
 
   public LoginViewModel(HotelModel model)
   {
+    this.model = model;
     username = new SimpleStringProperty();
     password = new SimpleStringProperty();
-    this.model = model;
+    error = new SimpleStringProperty();
+    model.addPropertyChangeListener(this);
+  }
+
+  public void bindUsername(StringProperty property)
+  {
+    property.bindBidirectional(username);
+  }
+
+  public void bindPassword(StringProperty property)
+  {
+    property.bindBidirectional(password);
+  }
+
+  public void bindError(StringProperty property)
+  {
+    property.bind(error);
+  }
+
+  @Override public void propertyChange(PropertyChangeEvent evt)
+  {
+
   }
 }
