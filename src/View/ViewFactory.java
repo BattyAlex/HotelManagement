e0da7bdb67491e0761e2982420fc137f56cd84f0
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-05-10 12:38:58 +0200

    murder

--- src/View/ViewFactory.java
+++ src/View/ViewFactory.java
@@ -2,11 +2,15 @@ package View;
 
 import Model.Room;
 import ViewModel.ViewModelFactory;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Region;
 
+import java.io.IOException;
 import java.security.PublicKey;
 
 public class ViewFactory
 {
+  private static final String LOGIN = "login";
   private final ViewHandler viewHandler;
   private final ViewModelFactory viewModelFactory;
   private LoginViewController loginViewController;
@@ -22,4 +26,34 @@ public class ViewFactory
     reservationViewController = null;
   }
 
+  public Region loadLoginView()
+  {
+    if(loginViewController == null)
+    {
+      FXMLLoader loader = new FXMLLoader();
+      loader.setLocation(getClass().getResource("LoginView.fxml"));
+      try
+      {
+        Region root = loader.load();
+        loginViewController = loader.getController();
+        loginViewController.init(viewHandler, viewModelFactory.getLoginViewModel(), root);
+      }
+      catch (IOException e)
+      {
+        System.out.println("Login View failed to load");
+        e.printStackTrace();
+      }
+    }
+    return loginViewController.getRoot();
+  }
+
+  public Region load(String id)
+  {
+    Region root = switch (id)
+    {
+      case LOGIN -> loadLoginView();
+      default -> throw new IllegalArgumentException("Unknown view: " + id);
+    };
+    return root;
+  }
 }
