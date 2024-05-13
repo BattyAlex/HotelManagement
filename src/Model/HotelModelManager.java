e1c2ef561478a8026ffd1f511d9277e257c489d6
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-05-13 10:37:23 +0200

    Update HotelModelManager.java

--- src/Model/HotelModelManager.java
+++ src/Model/HotelModelManager.java
@@ -13,12 +13,21 @@ public class HotelModelManager implements HotelModel
   private PropertyChangeSupport support;
   private HotelClient client;
 
+  /**
+   * Manages the model login for the hotel and handles operations such as user login and notifications for changes in property value
+   */
   public HotelModelManager(HotelClient client)
   {
     this.client = client;
     this.support = new PropertyChangeSupport(this);
   }
 
+  /**
+   * Attempts to log in a user with the specified username and password.
+   * Notifies listeners of the result of the login attempt.
+   * @param username the username of the staff member trying to log in.
+   * @param password the password of the staff member trying to log in.
+   */
 
   @Override public void tryLogin(String username, String password)
   {
@@ -42,17 +51,33 @@ public class HotelModelManager implements HotelModel
   {
   }
 
+  /**
+   * Adds a PropertyChangeListener to the list of listeners to be notified of property changes
+   * @param listener the PropertyChangeListener to be added.
+   */
+
   @Override public void addPropertyChangeListener(
       PropertyChangeListener listener)
   {
     support.addPropertyChangeListener(listener);
   }
 
+  /**
+   * Removes a PropertyChangeListener to the list of listeners.
+   * @param listener the PropertyChangeListener to be removed.
+   */
+
   @Override public void removePropertyChangeListener(
       PropertyChangeListener listener)
   {
     support.removePropertyChangeListener(listener);
   }
+
+  /**
+   * Method is called when a PropertyChangeEvent is about to get fired.
+   * @param evt the PropertyChangeEvent object containing the event details.??
+   */
+
   public void propertyChange(PropertyChangeEvent evt)
   {
 
