9c2bf17b9b21315f1cbd4bb57d657f117304d19d
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-05-13 15:05:21 +0200

    docs

--- src/Model/HotelModel.java
+++ src/Model/HotelModel.java
@@ -2,10 +2,30 @@ package Model;
 
 import java.beans.PropertyChangeListener;
 
+/**
+ * This interface defines the functionality for a hotel management system model
+ */
 public interface HotelModel
 {
+  /**
+   * Attempts to log in a user with the provided username and password
+   * @param username The username of the user attempting to log in
+   * @param password The password of the user attempting to log in
+   */
   void tryLogin(String username, String password);
+  /**
+   * Exits the client session and performs any necessary clean up operations
+   */
   void exitClient();
+  /**
+   * Adds the property change listener to the model
+   * Listeners are notified of changes to properties within the hotel model
+   * @param listener The property change listener to add
+   */
   void addPropertyChangeListener(PropertyChangeListener listener);
+  /**
+   * Removes the property change listener from the model
+   * @param listener The property change listener to remove.
+   */
   void removePropertyChangeListener(PropertyChangeListener listener);
 }
