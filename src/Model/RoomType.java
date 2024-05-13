e3a993a88c938c1c74baa27fa7028e232111e7b5
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-05-13 14:23:40 +0200

    docs

--- src/Model/RoomType.java
+++ src/Model/RoomType.java
@@ -2,13 +2,20 @@ package Model;
 
 import java.util.HashMap;
 
+/**
+ * This class represents a room type in a hotel management system.
+ */
 public class RoomType
 {
   private String key;
   private double price;
   private static HashMap<String, RoomType> instances = new HashMap<>();
 
-
+  /**
+   * Constructs a RoomType instance with a specified key and price.
+   * @param key A unique string key identifying the room type.
+   * @param price The price associated with this room type
+   */
   public RoomType(String key, double price)
   {
     this.key = key;
@@ -20,6 +27,13 @@ public class RoomType
     price = 0;
   }
 
+  /**
+   * Provides a global access point to get a RoomType instance by key and price.
+   * If the instance does not exist, it is created and returned. If it exists, the price is updated.
+   * @param key The unique for the room type
+   * @param price The price to set or update for the room type
+   * @return The RoomType instance corresponding to the provided key
+   */
   public static RoomType getInstance(String key, double price)
   {
     if (!instances.containsKey(key))
@@ -31,11 +45,19 @@ public class RoomType
     return instances.get(key);
   }
 
+  /**
+   * Sets the price of the room
+   * @param price the new price to set
+   */
   public void setPrice(double price)
   {
     this.price = price;
   }
 
+  /**
+   * Gets the type of key
+   * @return the key
+   */
   public String getType()
   {
     return key;
