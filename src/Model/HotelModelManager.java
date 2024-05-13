ef329c282aff4402071ec7ddc87dc2478136a6ba
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-05-13 10:25:00 +0200

    comments

--- src/Model/HotelModelManager.java
+++ src/Model/HotelModelManager.java
@@ -5,17 +5,28 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 
-public class HotelModelManager implements HotelModel
+/**
+ * Manages the model login for the hotel and handles operations such as user login and notifications for changes in property value
+ */
+public class  HotelModelManager implements HotelModel
 {
   private Staff staff;
   private PropertyChangeSupport support;
 
+  /**
+   * Constructor for HotelModelManager, initializes the support for property change events.
+   */
   public HotelModelManager()
   {
     this.support = new PropertyChangeSupport(this);
   }
 
-
+  /**
+   * Attempts to log in a user with the specified username and password.
+   * Notifies listeners of the result of the login attempt.
+   * @param username the username of the staff member trying to log in.
+   * @param password the password of the staff member trying to log in.
+   */
   @Override public void tryLogin(String username, String password)
   {
     staff = new Staff(username, password);
@@ -38,17 +49,30 @@ public class HotelModelManager implements HotelModel
   {
   }
 
+  /**
+   * Adds a PropertyChangeListener to the list of listeners to be notified of property changes
+   * @param listener the PropertyChangeListener to be added.
+   */
   @Override public void addPropertyChangeListener(
       PropertyChangeListener listener)
   {
     support.addPropertyChangeListener(listener);
   }
 
+  /**
+   * Removes a PropertyChangeListener to the list of listeners.
+   * @param listener the PropertyChangeListener to be removed.
+   */
   @Override public void removePropertyChangeListener(
       PropertyChangeListener listener)
   {
     support.removePropertyChangeListener(listener);
   }
+
+  /**
+   * Method is called when a PropertyChangeEvent is about to get fired.
+   * @param evt the PropertyChangeEvent object containing the event details.??
+   */
   public void propertyChange(PropertyChangeEvent evt)
   {
 
