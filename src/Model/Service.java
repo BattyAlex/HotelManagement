dcef637dac9c28870e96c471efdd74248510215e
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-05-13 14:50:13 +0200

    docs

--- src/Model/Service.java
+++ src/Model/Service.java
@@ -1,31 +1,55 @@
 package Model;
 
+/**
+ * Represents a service that can be offered as part of a hotel reservation
+ */
 public class Service
 {
   private String name;
   private double price;
 
+  /**
+   * Constructs a new service with the specific name and price
+   * @param name The name of the service
+   * @param price The price of the service
+   */
   public Service(String name, double price)
   {
     this.name = name;
     this.price = price;
   }
 
+  /**
+   * Retrieves the name of the service
+   * @return The name of the service
+   */
   public String getName()
   {
     return name;
   }
 
+  /**
+   * Sets the name of the service
+   * @param name The new name of the service
+   */
   public void setName(String name)
   {
     this.name = name;
   }
 
+  /**
+   * Retrieves the price of the service
+   * @return  The price of the service
+   */
   public double getPrice()
   {
     return price;
   }
 
+  /**
+   * Sets the price of the service
+   * @param price The new name of the service
+   */
   public void setPrice(double price)
   {
     this.price = price;
