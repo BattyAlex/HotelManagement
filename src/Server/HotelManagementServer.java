3f14ff0520df5eec66cf04895ed57016cb8ada13
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-05-14 10:42:31 +0200

    Update HotelManagementServer.java

--- src/Server/HotelManagementServer.java
+++ src/Server/HotelManagementServer.java
@@ -4,8 +4,19 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * The HotelManagementServer class sets up a servet that listens for client connections
+ * on port 8080 and uses a UPD broadcaster to communicate with clients.
+ */
+
 public class HotelManagementServer
 {
+  /**
+   * The main method that serves as the entry point of the application.
+   * It initializes the server socket and UDP broadcaster, and continuously listens for client connections.
+   * @param args Command-line arguments
+   * @throws IOException If an I/O error occurs when opening the socket.
+   */
   public static void main(String[] args) throws IOException
   {
     ServerSocket serverSocket = new ServerSocket(8080);
