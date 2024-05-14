5c55537b193f742c80cf9de881ee0e9c9bec80b1
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-05-14 10:05:05 +0200

    Update HotelManagementCommunicator.java

--- src/Server/HotelManagementCommunicator.java
+++ src/Server/HotelManagementCommunicator.java
@@ -6,6 +6,10 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+/**
+ * The {@code HotelManagementCommunicator} class implements the Runnable interface to handle
+ * communication with the hotel management system
+ */
 
 public class HotelManagementCommunicator implements Runnable
 {
@@ -15,12 +19,24 @@ public class HotelManagementCommunicator implements Runnable
   private BufferedReader reader;
   private PrintWriter writer;
 
+  /**
+   * Constructs a new {@code HotelManagementCommunicator} with the specified socket and UDP broadcaster.
+   * @param socket  the socket for communication
+   * @param broadcaster  the UDP broadcaster for message broadcasting
+   */
+
   public HotelManagementCommunicator(Socket socket, UDPBroadcaster broadcaster)
   {
     this.socket = socket;
     this.broadcaster = broadcaster;
     this.gson = new Gson();
   }
+
+  /**
+   * Handles communication over the socket. This method listens for incoming messages, processes them
+   * and sends appropriate responses. It runs in a loop until the connection is closed or an error occurs.
+   * @throws IOException if an I/0 error occurs while communicating.
+   */
 
   private void communicate() throws IOException
   {
@@ -69,6 +85,11 @@ public class HotelManagementCommunicator implements Runnable
       }
     }
   }
+
+  /**
+   * Runs the communication process. The method is invoked when the thread is started.
+   * It handles any exception that may occur.
+   */
   @Override public void run()
   {
     try
