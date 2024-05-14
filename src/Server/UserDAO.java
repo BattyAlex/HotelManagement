8c71a3024d22ae7fc3a6ababf45d76c884279231
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-05-14 10:54:00 +0200

    user

--- src/Server/UserDAO.java
+++ src/Server/UserDAO.java
@@ -4,14 +4,28 @@ import Model.Staff;
 
 import java.sql.*;
 
+/**
+ * The UserDAO class handles the data access operations related to the User entity
+ * It extends the DatabaseHandlerFactory class and implements the Singleton pattern.
+ */
 public class UserDAO extends DatabaseHandlerFactory
 {
   private static UserDAO instance;
+
+  /**
+   * Private constructor to prevent instantiation
+   * Registers the postgresSQL driver
+   * @throws SQLException If an error occurs during driver registration
+   */
   private UserDAO() throws SQLException
   {
     DriverManager.registerDriver(new org.postgresql.Driver());
   }
 
+  /**
+   * Provides the singleton instance of UserDAO
+   * @return The Singleton instance of UserDAO
+   */
   public static UserDAO getInstance()
   {
     try
@@ -29,6 +43,11 @@ public class UserDAO extends DatabaseHandlerFactory
     }
   }
 
+  /**
+   * Retrieves a staff member based on the provided username.
+   * @param username The username of the Staff member to retrieve
+   * @return A Staff object if a matching username is found, otherwise null
+   */
   public Staff getStaffBasedOnUsername(String username)
   {
     try(Connection connection = super.establishConnection())
