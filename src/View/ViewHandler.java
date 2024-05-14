24c04c838788485948342d5e4fb5ac5a846cfed2
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-05-14 14:00:10 +0200

    Update ViewHandler.java

--- src/View/ViewHandler.java
+++ src/View/ViewHandler.java
@@ -5,22 +5,44 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.Region;
 
+/**
+ * The ViewHandler class is responsible for managing the different views in the application
+ * It handles the loading, displaying, and closing of views
+ */
+
 public class ViewHandler
 {
   private ViewFactory viewFactory;
   private Scene currentScene;
   private Stage primaryStage;
 
+  /**
+   * Constructs a ViewHandler with the specified ViewModelFactory.
+   *
+   * @param viewModelFactory The factory for creating view models.
+   */
+
   public ViewHandler(ViewModelFactory viewModelFactory)
   {
     this.viewFactory = new ViewFactory(this, viewModelFactory);
     this.currentScene = new Scene(new Region());
   }
+
+  /**
+   * Starts the application by displaying the primary stage with the login view
+   *
+   * @param primaryStage The primary stage of the application.
+   */
   public void start(Stage primaryStage)
   {
     this.primaryStage = primaryStage;
     openView(ViewFactory.LOGIN);
   }
+
+  /**
+   * Opens the specified view by loading it from the ViewFactory
+   * @param id The ID of the view to load
+   */
   public void openView(String id)
   {
     Region root = viewFactory.load(id);
@@ -34,6 +56,10 @@ public class ViewHandler
     primaryStage.sizeToScene();
     primaryStage.show();
   }
+
+  /**
+   * Closes the current view by closing the primary stage
+   */
   public void closeView()
   {
     primaryStage.close();
