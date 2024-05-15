8276bb54806b2e398121793e64959caef097c2d3
BattyAlex <Alexandriaahansen@gmail.com>
2024-05-15 12:36:37 +0200

    reservations view

--- src/View/ReservationViewController.java
+++ src/View/ReservationViewController.java
@@ -1,14 +1,31 @@
 package View;
 
 import ViewModel.ReservationViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 
 import javax.swing.plaf.synth.Region;
+import java.awt.*;
 
 public class ReservationViewController
 {
   private Region root;
   private ReservationViewModel reservationViewModel;
   private ViewHandler viewHandler;
+  @FXML private TextField firstName;
+  @FXML private TextField lastName;
+  @FXML private TextField cardInfo;
+  @FXML private Label amenities;
+  @FXML private DatePicker startDate;
+  @FXML private DatePicker endDate;
+  @FXML private ChoiceBox roomNumber;
+  @FXML private Checkbox roomService;
+  @FXML private Checkbox airportTransport;
+  @FXML private Checkbox breakfast;
+  @FXML private Checkbox lunch;
+  @FXML private Checkbox dinner;
+  @FXML private Checkbox wellness;
 
   public void init(ViewHandler viewHandler, ReservationViewModel reservationViewModel, Region root)
   {
--- src/ViewModel/ReservationViewModel.java
+++ src/ViewModel/ReservationViewModel.java
@@ -1,13 +1,21 @@
 package ViewModel;
 
 import Model.HotelModel;
+import javafx.beans.property.StringProperty;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.time.LocalDate;
 
 public class ReservationViewModel implements PropertyChangeListener
 {
   private final HotelModel model;
+  private StringProperty firstName;
+  private StringProperty lastName;
+  private StringProperty cardInfo;
+  private LocalDate startDate;
+  private LocalDate endDate;
+  
 
   public ReservationViewModel(HotelModel model)
   {
