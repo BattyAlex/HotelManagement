2a51d46d1d0ac2eac2324914b1ce592ff13b6b30
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-05-21 15:14:26 +0200

    Update StartApplication.java

--- src/StartApplication.java
+++ src/StartApplication.java
@@ -11,8 +11,24 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * The StartApplication class serves as the entry point for the Hotel Management application
+ * It sets up the client, model, view model factory and view handler, and it manages the primary stage of the JavaFX application.
+ *
+ */
 public class StartApplication extends Application
 {
+  /**
+   * The start method is the main entry point for all JavaFX applications.
+   * It initializes the HotelClient, HotelModel, ViewModelFactory and ViewHandler
+   * and sets up the primary stage and its close request handler.
+   *
+   * @param primaryStage the primary stage for this application, onto which
+   * the application scene can be set.
+   * Applications may create other stages, if needed, but they will not be
+   * primary stages.
+   * @throws Exception if any error occurs during initialization.
+   */
 
   @Override public void start(Stage primaryStage) throws Exception
   {
@@ -26,6 +42,13 @@ public class StartApplication extends Application
       viewHandler.closeView();
     });
   }
+
+  /**
+   * The main method serves as the entry point for the Java application.
+   * It launches the JavaFX application.
+   *
+   * @param args the command line arguments.
+   */
 
   public static void main(String[] args)
   {
