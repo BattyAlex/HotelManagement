1d7c72de0518df783c71ecf2557144224320e466
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-05-21 14:17:50 +0200

    Update CleaningViewModel.java

--- src/ViewModel/CleaningViewModel.java
+++ src/ViewModel/CleaningViewModel.java
@@ -9,12 +9,24 @@ import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+/**
+ * The CleaningViewModel class is responsible for managing the cleaning state of rooms
+ * It implements PropertyChangeListener to respond to property change events from the HotelModel
+ */
+
 public class CleaningViewModel implements PropertyChangeListener
 {
   private final HotelModel model;
   private StringProperty roomInfo;
   private StringProperty roomNumber;
   private StringProperty roomState;
+
+  /**
+   * Constructs a CleaningViewModel with the specified HotelModel.
+   * Initializes the room information properties and adds this ViewModel as a listener to the HotelModel.
+   *
+   * @param model the HotelModel to be associated with this ViewModel.
+   */
 
   public CleaningViewModel(HotelModel model)
   {
@@ -25,19 +37,41 @@ public class CleaningViewModel implements PropertyChangeListener
     roomState = new SimpleStringProperty("");
   }
 
+  /**
+   * Binds the specified StringProperty to the roomInfo property of this ViewModel.
+   * @param property  the StringProperty to bind to roomInfo
+   */
+
   public void bindRoomInfo(StringProperty property)
   {
     property.bind(roomInfo);
   }
+
+  /**
+   * Binds the specified StringProperty to the roomNUmber property of this ViewModel.
+   *
+   * @param property the StringProperty to bind to roomNumber.
+   */
   public void bindRoomNumber(StringProperty property)
   {
     property.bind(roomNumber);
   }
 
+  /**
+   * Binds the specified StringProperty to the roomState property of this ViewModel.
+   *
+   * @param property the StringProperty to bind to roomState.
+   */
+
   public void bindRoomState(StringProperty property)
   {
     property.bind(roomState);
   }
+
+  /**
+   * Sets the state of the room to "CLEANED" and updates the HotelModel
+   * It also resets the fields of the ViewModel
+   */
 
   public void setCleaned()
   {
@@ -56,6 +90,11 @@ public class CleaningViewModel implements PropertyChangeListener
     }
   }
 
+  /**
+   * Sets the state of the room to "UNDER_CLEANING" and updates the HotelModel
+   * It also resets the fields of the ViewModel
+   */
+
   public void setUndergoingCleaning()
   {
     try
@@ -73,12 +112,24 @@ public class CleaningViewModel implements PropertyChangeListener
     }
   }
 
+  /**
+   * Resets the roomInfo, roomNumber and roomState properties to empty strings.
+   */
+
   public void resetFields()
   {
     roomInfo.set("");
     roomNumber.set("");
     roomState.set("");
   }
+
+  /**
+   * Responds to property change events from the HotelModel
+   * Updates the room information properties when a "Room Selected For Cleaning" event is received
+   * 
+   * @param evt A PropertyChangeEvent object describing the event source
+   *          and the property that has changed.
+   */
   @Override public void propertyChange(PropertyChangeEvent evt)
   {
     if(evt.getPropertyName().equals("Room Selected For Cleaning"))
